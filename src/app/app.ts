7b9c9badca8cf3165d6329cdf60751624d6eab8f
Quentin Geerts <quentin.geerts@bstorm.be>
2025-07-28 15:54:15 +0200

    feat: add layout components and styles for header, footer, main, and nav
    - Implemented header, footer, main, and nav components with respective HTML and SCSS files.
    - Defined global styles and variables for consistent theming.
    - Set up responsive layout with header, footer, and main content area.
    - Added links in the footer for personal and professional references.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './layout/header/header';
+import { Main } from './layout/main/main';
+import { Footer } from './layout/footer/footer';
+import { Nav } from './layout/nav/nav';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Main, Footer, Nav],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
